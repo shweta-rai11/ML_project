e357aa7d9245eacfdfa105123e302a55682f2e13
shweta-rai11 <raiishweta808@gmail.com>
2024-12-28 20:11:35 -0700

    model trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,22 +1,6 @@
 import sys
 from dataclasses import dataclass
 
-import numpy as np
-import pandas as pd
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder,StandardScaler
-
-from src.exception import CustomException
-from src.logger import logging
-import os
-from src.utlis import save_object
-import dill
-
-import sys
-from dataclasses import dataclass
-
 import numpy as np 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
@@ -28,7 +12,7 @@ from src.exception import CustomException
 from src.logger import logging
 import os
 
-#from src.utils import save_object
+from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
